8ad8c2fb1f0e277c00c5a075d25c25bbbef6434c
lalithrajr1034 <placement1034@gmail.com>
2025-02-13 18:26:38 +0530

    this is remainder

--- python_lab/program1.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-product_A_sales = [100, 120, 130, 150, 160, 180]
-product_B_sales = [80, 100, 90, 110, 120, 130]
-plt.bar(product_A_sales, product_B_sales)
-plt.title('Recommended Time Allocation for Academic and Career Preparation')
-plt.legend()
-plt.show()
