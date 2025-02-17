d369769a2b12bcb178b93ca70166b318d10df2d7
lalithrajr1034 <placement1034@gmail.com>
2025-02-17 23:55:11 +0530

    updated

--- 7_loop's.py
+++ 7_loop's.py
@@ -47,3 +47,78 @@ iterator=0
 while a>=iterator:
     print(number[iterator])
     iterator+=1
+#                        (or)
+
+number=[1,4,9,16,25,36,49,64,81,100]
+b=["lal","SADAE","lal","SADAE","lal","SADAE","lal","SADAE","lal","SADAE",]
+a=len(number)-1
+iterator=0
+while a>=iterator:
+    print(number[iterator],b[iterator])
+    iterator+=1
+
+#search for a number x in this tupple using loop   (1,4,9,16,25,36,49,64,81,100)
+
+number=(1,4,9,16,25,36,49,64,81,100)
+a=len(number)
+value=int(input())
+i=0
+while a>i:
+    if (number[i]==value):
+        print("Entered number",value,"Is found at index",i)   
+    i+=1
+
+# BREAK :-
+    #It helps to brek the loop 
+
+
+#CONTINUE:-
+    #This key word helps skip that loop and move on to next
+    
+i=0
+while i<=5:
+    if i==3:
+        i+=1
+        continue #BY USING THIS WE SKIPED 3
+    print(i)
+    i+=1    
+
+
+i=0
+while i<=20:
+    if i%2==0:
+        i+=1
+        continue
+    print(i)
+    i+=1    
+
+
+
+
+
+
+
+#FOR LOOP........................................................................................................................................
+
+index=(1,2,3,4,5,6,7,8,9,9,3,54,56,67,78)
+value=int(input("Enter the number:"))
+i=0
+for a in index:
+    if a==value:
+        print("I found this in index",i)
+    i+=1    
+
+
+
+#------>>> range
+
+s=range(2,10,3)
+for i in s:
+    print(i)
+    
+
+   # (or)
+
+for d in range(  2 ,    20 ,  2  ):   
+#             start   stop   step
+    print(d)
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-pen=int(input())
-
-if pen==25:
-    print("I will buy a pen")    
-
-else:
-    print("There is no pen")
