502f3aad2aa8b11b83e5f7ab698b8147fca637fc
lalithrajr1034 <placement1034@gmail.com>
2025-02-27 23:28:31 +0530

    updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,13 @@
-c=7%-2
-print(c)
+a = [1, 3,  5,  7,  9]
+i = 0
+c = len(a)
+found_even = True  # Flag to check if even numbers exist
+
+while i < c:
+    if a[i] % 2 == 0:
+        print("Even number:", a[i])
+        found_even = False
+    i += 1
+
+if found_even:
+    print("No even numbers found.")
