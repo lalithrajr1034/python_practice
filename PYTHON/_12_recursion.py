e55f10b20687fcdf66bc7d26e9e2095101004ca6
lalithrajr1034 <placement1034@gmail.com>
2025-03-11 22:36:29 +0530

    updated

--- PYTHON/_12_recursion.py
+++ PYTHON/_12_recursion.py
@@ -3,24 +3,20 @@
 
 def recursion(b):
     if b==0: # base case  "This helps to stop the recursion"
-        return
+        return 0
     print(b)
     recursion(b-1)
+
 recursion(6)    
 
 
-
-
-
-
-
-
-
+#recursion through factorial
 
 def factorial(n): 
     if n == 0 or n == 1: 
         return 1
     else:
-        return n * factorial(n - 1)  
+        return n * factorial(n - 1)            # n!=(n-1)!*n
+    
 num=int(input("Enter the factotial"))
 print(f"Factorial of {num} is {factorial(num)}")#f-string
