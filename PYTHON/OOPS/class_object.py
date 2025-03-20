603f2856f53333c32ece377cff8ef6cd05577709
lalithraj1034 <placement1034@gmail.com>
2025-03-21 00:22:07 +0530

    updated

--- PYTHON/OOPS/class_object.py
+++ PYTHON/OOPS/class_object.py
@@ -1,7 +1,19 @@
-# class 
-class Lalith:
-    a="lalith"
+# CLASS
 
-#object
-s1=Lalith()
-print(s1.a)    
+# Data          +   Functions   =  Class
+# Attributes        Methods       
+# Property          Behavior
+
+#Data :-  like discription about the element (ex: colour, wheels , brand)
+#Function:-it is like a behaviour of the element (ex: calculate , Show milage)
+
+#NOTE :- class name should be in pascal case = ThisIsPascalCase
+#NOTE :- Function should be in snake case = this_is_snake_case
+
+#...................................
+#CONSTRUCTOR
+
+ 
+class Atm:
+
+
