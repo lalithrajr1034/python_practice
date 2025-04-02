b89294d4ec588b44b5f6cc1c917ba241c80c506b
lalithraj1034 <placement1034@gmail.com>
2025-04-02 20:33:59 +0530

    updated

--- PYTHON/OOPS/class_object.py
+++ PYTHON/OOPS/class_object.py
@@ -10,10 +10,29 @@
 #NOTE :- class name should be in pascal case = ThisIsPascalCase
 #NOTE :- Function should be in snake case = this_is_snake_case
 
+class lalith:
+    a="allalal"    
+    def hi():
+        print("hello")
+lal=lalith()       
+print(lal)  
+
 #...................................
-#CONSTRUCTOR
+#CONSTRUCTOR(__init__())
+#Constructor is a special method that automatically execute when a object of a class is ctreated 
+class constructor:
+    def __init__(self,myname):#Self parameter is pointig towards object 
+        self.name=myname #self = objectname.attribute
 
- 
-class Atm:
+ob=constructor()
+print(constructor.name)
 
 
+
+class l:
+    def __init__(self):
+        print()
+    def lalith(self):
+        self.a=4
+        print(a)
+l.lalith()        
--- PYTHON/OOPS/tempCodeRunnerFile.py
+++ PYTHON/OOPS/tempCodeRunnerFile.py
@@ -1,7 +1,6 @@
-#Constructor is a special method that automatically execute when a object of a class is ctreated 
-class constructor:
-    def __init__(self,myname):#Self parameter is pointig towards object 
-        self.name=myname #self = objectname.attribute
-
-ob=constructor("hi")
-print(ob.name)
+class l:
+    def __init__(self):
+        pass
+    def lalith():
+        print("Hi lalith")
+l.lalith() 
