ea860acb2972892e36cf8891d74fa7b920d8dd4d
lalithrajr1034 <placement1034@gmail.com>
2025-05-05 22:57:59 +0530

    updated

---
+++ PYTHON/DSA/NON_LINEAR/TREE/1_intruduction.py
@@ -0,0 +1,30 @@
+#TREE:
+  #Tree is a non primitive, non linear data structure it helps to represent a hierarchical realation ship among elements
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Brath First Search
+
+
+#Deapth First Search 
+   #In order traversal [7,12,14,15,20,23,27,88]
+   #Pre order traversal [15,12,7,14,27,20,23,88]
+   #Post orser traversal [7,14,12,23,20,88,27,15]
---
+++ PYTHON/DSA/NON_LINEAR/TREE/2_binary_search_tree.py
@@ -0,0 +1,4 @@
+#Binary search tree
+   #it is a type of a Tree in  which it contain lower value's on left side and greater value is in right side
+
+
