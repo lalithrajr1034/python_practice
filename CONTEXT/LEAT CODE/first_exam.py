0e024900629425a08439f0d200ea39cec27d2472
lalithrajr1034 <placement1034@gmail.com>
2025-05-18 20:36:15 +0530

    updated

--- CONTEXT/LEAT CODE/first_exam.py
+++ CONTEXT/LEAT CODE/first_exam.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, lis ,target):
-        for i in range(0,len(lis)):
+        for i in range(0,len(lis)-2):
             if lis[i]==target:
                 print(f"[{i}]")
-            elif lis[i]+lis[i+1]==target:
+            elif (lis[i]+lis[i+1])==target:
                 print(f"[{i},{i+1}]") 
-            elif lis[i]+lis[i+2]==target:
+            elif (lis[i]+lis[i+2])==target:
                 print(f"[{i},{i+2}]")         
 
 
@@ -14,3 +14,13 @@ nums=[2,7,11,15]
 target=9
 obj=Solution()
 b=obj.twoSum(nums,target)
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}  # Stores value -> index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
--- FRAME_WORK/NUMPY/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-a=np.arange((1,11),(1,11))
-print(a)
