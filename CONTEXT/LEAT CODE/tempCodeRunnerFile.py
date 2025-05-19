0a1e445c161bf07d8b3305a3435b706e0190accc
lalithrajr1034 <placement1034@gmail.com>
2025-05-19 06:17:09 +0530

    updated

--- CONTEXT/LEAT CODE/tempCodeRunnerFile.py
+++ CONTEXT/LEAT CODE/tempCodeRunnerFile.py
@@ -1,16 +1,1 @@
-class Solution:
-    def twoSum(self, lis ,target):
-        for i in range(0,len(lis)-2):
-            if lis[i]==target:
-                print(f"[{i}]")
-            elif (lis[i]+lis[i+1])==target:
-                print(f"[{i},{i+1}]") 
-            elif (lis[i]+lis[i+2])==target:
-                print(f"[{i},{i+2}]")         
-
-
-
-nums=[2,7,11,15]
-target=9
-obj=Solution()
-b=obj.twoSum(nums,target)
+print(lis) 
