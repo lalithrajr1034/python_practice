bf126100833fc413ae0d75259b993e4dd257a0e7
lalithrajr1034 <placement1034@gmail.com>
2025-05-20 22:10:08 +0530

    updated

--- PYTHON/DSA/NON_LINEAR/TREE/2_binary_search_tree.py
+++ PYTHON/DSA/NON_LINEAR/TREE/2_binary_search_tree.py
@@ -3,36 +3,5 @@
 
 
 # NOTE: binary search tree dosenot suppert duplicate value
-class Node:
-    def __init__(self, data):
-        self.data = data  # Store data
-        self.next = None  # Reference to next node
 
-class LinkedList:
-    def __init__(self):
-        self.head = None  # Start of the list
 
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node  # Set first node
-            return
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
-
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" -> ")
-            current = current.next
-        print("None")
-
-# Usage
-ll = LinkedList()
-ll.append(10)
-ll.append(20)
-ll.append(30)
-
-ll.display()  # Output: 10 -> 20 -> 30 -> None
