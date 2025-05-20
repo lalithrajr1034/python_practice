f43d41d89c8d480a4a6c0e91e95982542943cf91
lalithrajr1034 <placement1034@gmail.com>
2025-05-20 22:08:51 +0530

    updated

--- PYTHON/DSA/NON_LINEAR/TREE/1_intruduction.py
+++ PYTHON/DSA/NON_LINEAR/TREE/1_intruduction.py
@@ -1,5 +1,5 @@
 #TREE:
-  #Tree is a non primitive, non linear data structure it helps to represent a hierarchical realation ship among elements
+  #Tree is a hirerchical, non primitive, non linear data structure it helps to represent a parent and child relationship
 
 
 
@@ -25,6 +25,6 @@
 
 
 #Deapth First Search 
-   #In order traversal [7,12,14,15,20,23,27,88]
-   #Pre order traversal [15,12,7,14,27,20,23,88]
-   #Post orser traversal [7,14,12,23,20,88,27,15]
+   #In order traversal [7,12,14,15,20,23,27,88]     root node is in middle and left-elements are left and right-elemrnts are right
+   #Pre order traversal [15,12,7,14,27,20,23,88]    left subtree is firt then right subtree
+   #Post orser traversal [7,14,12,23,20,88,27,15]   right subtree is first then left subtee 
