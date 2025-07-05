abdfd9ad7dbf706b45be4f1227d05f2089e444b5
lalithrajr1034 <placement1034@gmail.com>
2025-07-05 09:11:26 +0530

    updated

--- FRAME_WORK/MYSQL/1_introduction.py
+++ FRAME_WORK/MYSQL/1_introduction.py
@@ -1,12 +1,21 @@
 # From this we will interact between sql and python 
 # To install "pip install mysql-connector-python"
 
-import mysql.connector
+import mysql.connector as mydb
 
-var1 = mysql.connector.connect(
+var1 = mydb.connect(
     password='lalithrajr838@',
     host='localhost',
     user='root',
     database='python_practice'
 )
 
+# using cursor 
+cur = var1.cursor()
+cur.execute("""show databases""")
+
+for i in cur:
+    print(i)
+    
+
+                                                  
--- FRAME_WORK/MYSQL/tempCodeRunnerFile.py
+++ FRAME_WORK/MYSQL/tempCodeRunnerFile.py
@@ -1,11 +1,2 @@
-# From this we will interact between sql and python 
-# To install "pip install mysql-connector-python"
-
-import mysql.connector
-
-var1 = mysql.connector.connect(
-    password='lalithrajr838@',
-    host='localhost',
-    user='root',
-    database='python'
-)
+cur.close()
+var1.close()   
