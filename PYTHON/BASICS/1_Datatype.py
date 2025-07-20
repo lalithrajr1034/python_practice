a33d7d55b520042991a597aec26955f4bb1f3944
lalithrajr1034 <placement1034@gmail.com>
2025-07-20 21:34:20 +0530

    updated

---
+++ PYTHON/BASICS/1_Datatype.py
@@ -0,0 +1,9 @@
+
+""" DATA TYPE """
+# | Type                                            | Size in Bytes |
+# | ----------------------------------------------- | ------------- |
+# | `int8`,       `uint8`,   `bool`                 | 1 byte        |
+# | `int16`,      `uint16`,  `float16`              | 2 bytes       |
+# | `int32`,      `uint32`,  `float32`              | 4 bytes       |
+# | `int64`,      `uint64`,  `float64`, `complex64` | 8 bytes       |
+# | `complex128`, `float128`                        | 16 bytes      |
