ce9b70bb5dddead95d7451af2b58f1a372f3e79e
lalithrajr1034 <placement1034@gmail.com>
2025-07-20 22:48:50 +0530

    updated

--- PYTHON/BASICS/tempCodeRunnerFile.py
+++ PYTHON/BASICS/tempCodeRunnerFile.py
@@ -1,8 +1,4 @@
-def advanced(data):
-    print(data)
-    def wrapper(val):
-        print(f' this is the advanced topic {val}')
-    return wrapper
-
-a = advanced("hi lalith")("my name is lalith")
-print(a)
+a = range(101)
+b = range(100,201)
+for a,b in zip(a,b):
+    print(a,b)
