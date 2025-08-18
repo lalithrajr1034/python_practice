0fcb9700e3e9002473d66ccb05279691097b7e84
lalithrajr1034 <placement1034@gmail.com>
2025-08-18 20:55:36 +0530

    Updated

--- PYTHON/DSA/LINEAR/LINKED_LIST/z.py
+++ PYTHON/DSA/LINEAR/LINKED_LIST/z.py
@@ -17,11 +17,18 @@ class linked_list:
     def append(self, data)->None:
         new_node = node(data)
         var = self.head
-        while var.pointer != None:
+        while var != None:
             var = var.pointer    
         var = new_node
         self.count += 1
-
+    def traversal(self):
+        val = self.head
+        while val != None:
+            var = var.pointer
+            print(var)
+            print("Length of the linked list", self.count)
+    a = traversal
+    a()     
 obj = linked_list(3)
 obj.head_append
 ob = linked_list(5)
