93ecc8a90ce6e0c7eec404c73957af69fce07e7c
lalithrajr1034 <placement1034@gmail.com>
2025-08-29 21:17:58 +0530

    Updated

--- CONTEXT/LEAT CODE/main.py
+++ CONTEXT/LEAT CODE/main.py
@@ -1,13 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i in range(nums):
-            for j in range(nums):
-                if nums[i]+nums[j] == target:
-                    return [nums[i],nums[j]]
-                
-li = [3,2,4]
-# a = Solution()
-# print(a.twoSum(li, 6))
+val = {"hi":"laltih",
+       "name":"lalith raj r",
+       "village":"besagarahalli"}
 
-for i in range(len(li)):
-    print(i)
+print(val.get())
---
+++ MODULES/NUMPY/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+result = ""
+a = "pwwkew"
+for i in a:
+    if i not in result:
+        result+=i
+print(result) 
