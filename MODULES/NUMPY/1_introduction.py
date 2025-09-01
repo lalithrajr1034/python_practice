7b1d8a498346f6ba99a9891e81e9fdeb0e7c2f29
lalithrajr1034 <placement1034@gmail.com>
2025-09-01 18:44:27 +0530

    Updated

--- MODULES/NUMPY/1_introduction.py
+++ MODULES/NUMPY/1_introduction.py
@@ -1,2 +1,21 @@
 # Numpy is a created to full fill the speed requirednment in python with C-ectencion,  
 # Numpy works on n- dimendional aray like array in C, it can store a only one data type 
+
+
+result = ""
+a = "pwwkew"
+for i in a:
+    if i not in result:
+        result+=i
+print(result)        
+        
+        
+s = "lalithrajr"
+
+result = ""
+for ch in s:
+    if ch not in result:
+        result += ch
+
+print(result)
+        
